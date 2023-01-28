3ae9d787f1ca252c1db3747c4672e5b45bb34754
Alessandro Rapiti <alessandro.rapiti@agic.cloud>
2023-01-28 18:12:19 +0100

    table test with child content

---
+++ src/Extensions/Web/Rystem.Web.Components/Contents/ITable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rystem.Web.Components.Contents
+{
+    public interface ITable
+    {
+        public string X { get; set; }
+    }
+}
---
+++ src/Extensions/Web/Rystem.Web.Components/Contents/Table.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Rystem.Web.Components.Customization;
+
+namespace Rystem.Web.Components.Contents
+{
+    public partial class Table<T> : ITable
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        public string X { get; set; }
+    }
+}
