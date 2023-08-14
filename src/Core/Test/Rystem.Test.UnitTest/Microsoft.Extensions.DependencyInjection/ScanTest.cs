d124479b4885fbc474668054fd2bc052e2d3354a
Alessandro Rapiti <alessandro.rapiti@agic.cloud>
2023-08-14 17:36:39 +0200

    new test for scan

--- src/Core/Test/Rystem.Test.UnitTest/Microsoft.Extensions.DependencyInjection/ScanTest.cs
+++ src/Core/Test/Rystem.Test.UnitTest/Microsoft.Extensions.DependencyInjection/ScanTest.cs
@@ -9,10 +9,12 @@ namespace Rystem.Test.UnitTest.DependencyInjection
         [Fact]
         public void Run()
         {
-            var service = new ServiceCollection();
-            service.ScanDependencyContext(ServiceLifetime.Scoped);
-            var actualService = service.Last();
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.ScanDependencyContext(ServiceLifetime.Scoped);
+            serviceCollection.ScanDependencyContext(ServiceLifetime.Scoped);
+            var actualService = serviceCollection.Last();
             Assert.Equal(ServiceLifetime.Singleton, actualService.Lifetime);
+            Assert.Equal(1, serviceCollection.Count);
         }
     }
 }
