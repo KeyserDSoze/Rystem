0f6432efd815081a3552fc88f38eff5bdb9ce598
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2023-08-14 12:18:10 +0200

    scan from type and from types

---
+++ src/Core/Rystem/Microsoft.Extensions.DependencyInjection/Scanner/ServiceCollectionExtesions_FromType.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static partial class ServiceCollectionExtesions
+    {
+        public static IServiceCollection ScanFromType<T, TScanAssemblyRetriever>(
+            this IServiceCollection services,
+            ServiceLifetime lifetime)
+            => services.Scan(typeof(T), lifetime, typeof(TScanAssemblyRetriever).Assembly);
+        public static IServiceCollection ScanFromType(
+            this IServiceCollection services,
+            Type serviceType,
+            Type scanAssemblyRetriever,
+            ServiceLifetime lifetime)
+            => services.Scan(serviceType, lifetime, scanAssemblyRetriever.Assembly);
+        public static IServiceCollection ScanFromType<TScanAssemblyRetriever>(
+           this IServiceCollection services,
+           ServiceLifetime lifetime)
+            => services.Scan(lifetime, typeof(TScanAssemblyRetriever).Assembly);
+    }
+}
---
+++ src/Core/Rystem/Microsoft.Extensions.DependencyInjection/Scanner/ServiceCollectionExtesions_FromType_2.cs
@@ -0,0 +1,21 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+    public static partial class ServiceCollectionExtesions
+    {
+        public static IServiceCollection ScanFromTypes<T, TScanAssemblyRetriever, TScanAssemblyRetriever2>(
+            this IServiceCollection services,
+            ServiceLifetime lifetime)
+            => services.Scan(typeof(T), lifetime, typeof(TScanAssemblyRetriever).Assembly, typeof(TScanAssemblyRetriever2).Assembly);
+        public static IServiceCollection ScanFromTypes(
+            this IServiceCollection services,
+            Type serviceType,
+            Type scanAssemblyRetriever,
+            Type scanAssemblyRetriever2,
+            ServiceLifetime lifetime)
+            => services.Scan(serviceType, lifetime, scanAssemblyRetriever.Assembly, scanAssemblyRetriever2.Assembly);
+        public static IServiceCollection ScanFromTypes<TScanAssemblyRetriever, TScanAssemblyRetriever2>(
+           this IServiceCollection services,
+           ServiceLifetime lifetime)
+            => services.Scan(lifetime, typeof(TScanAssemblyRetriever).Assembly, typeof(TScanAssemblyRetriever2).Assembly);
+    }
+}
