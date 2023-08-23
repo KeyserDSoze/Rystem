909111e7141021acb8d3156b2ad3d4256f875544
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2023-08-23 11:35:21 +0200

    new dependency injection and new wasm app

---
+++ src/Repository/RepositoryFramework.Test/RepositoryFramework.Wasm/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using RepositoryFramework.InMemory;
+using RepositoryFramework.Wasm;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddRepository<WeatherForecast, string>(repositoryBuilder =>
+{
+    repositoryBuilder.WithInMemory(inMemoryBuilder =>
+    {
+        inMemoryBuilder.PopulateWithRandomData();
+    });
+});
+
+await builder.Build().RunAsync();
