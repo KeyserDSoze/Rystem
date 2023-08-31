29006f098308dceb6ca2b4ed2cb71042a523a34e
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2023-08-31 16:17:17 +0200

    Default for state and reponse as task

--- src/Repository/RepositoryFramework.Abstractions/Models/State/State.cs
+++ src/Repository/RepositoryFramework.Abstractions/Models/State/State.cs
@@ -1,6 +1,4 @@
-﻿using System.Text.Json.Serialization;
-
-namespace RepositoryFramework
+﻿namespace RepositoryFramework
 {
     public static class State
     {
@@ -22,5 +20,24 @@ namespace RepositoryFramework
         public static State<T, TKey> Default<T, TKey>(bool isOk, Entity<T, TKey>? entity = default)
          where TKey : notnull
          => new(isOk, entity);
+
+        public static Task<State<T, TKey>> OkAsTask<T, TKey>(T value, TKey key)
+           where TKey : notnull
+           => Task.FromResult(new State<T, TKey>(true, new Entity<T, TKey>(value, key)));
+        public static Task<State<T, TKey>> OkAsTask<T, TKey>(Entity<T, TKey>? entity = default)
+            where TKey : notnull
+            => Task.FromResult(new State<T, TKey>(true, entity));
+        public static Task<State<T, TKey>> NotOkAsTask<T, TKey>(T value, TKey key)
+            where TKey : notnull
+            => Task.FromResult(new State<T, TKey>(false, new Entity<T, TKey>(value, key)));
+        public static Task<State<T, TKey>> NotOkAsTask<T, TKey>(Entity<T, TKey>? entity = default)
+         where TKey : notnull
+         => Task.FromResult(new State<T, TKey>(false, entity));
+        public static Task<State<T, TKey>> DefaultAsTask<T, TKey>(bool isOk, T value, TKey? key = default)
+            where TKey : notnull
+            => Task.FromResult(new State<T, TKey>(isOk, new Entity<T, TKey>(value, key)));
+        public static Task<State<T, TKey>> DefaultAsTask<T, TKey>(bool isOk, Entity<T, TKey>? entity = default)
+         where TKey : notnull
+         => Task.FromResult(new State<T, TKey>(isOk, entity));
     }
 }
--- src/Repository/RepositoryFramework.Api.Server/Authorization/Models/AuthorizedRepositoryResponse.cs
+++ src/Repository/RepositoryFramework.Api.Server/Authorization/Models/AuthorizedRepositoryResponse.cs
@@ -6,5 +6,9 @@
         public bool Success { get; set; }
         public static AuthorizedRepositoryResponse Ok() => new() { Success = true };
         public static AuthorizedRepositoryResponse NotOk() => new() { Success = false };
+        public static Task<AuthorizedRepositoryResponse> OkAsTask() => Task.FromResult(new AuthorizedRepositoryResponse() { Success = true });
+        public static Task<AuthorizedRepositoryResponse> NotOkAsTask() => Task.FromResult(new AuthorizedRepositoryResponse() { Success = false });
+        public static implicit operator bool(AuthorizedRepositoryResponse authorizedRepositoryResponse) => authorizedRepositoryResponse.Success;
+        public static implicit operator AuthorizedRepositoryResponse(bool success) => new() { Success = success };
     }
 }
