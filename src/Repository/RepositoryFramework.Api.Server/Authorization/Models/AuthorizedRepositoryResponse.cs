c9ad307c5a4c0f5ed020a75fdd4c787a457f1163
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2023-08-31 15:43:51 +0200

    Default Responses for AuthorizedRepositoryResponse

--- src/Repository/RepositoryFramework.Api.Server/Authorization/Models/AuthorizedRepositoryResponse.cs
+++ src/Repository/RepositoryFramework.Api.Server/Authorization/Models/AuthorizedRepositoryResponse.cs
@@ -4,5 +4,7 @@
     {
         public string? Message { get; set; }
         public bool Success { get; set; }
+        public static AuthorizedRepositoryResponse Ok() => new() { Success = true };
+        public static AuthorizedRepositoryResponse NotOk() => new() { Success = false };
     }
 }
