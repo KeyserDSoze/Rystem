c1b1149370d165b63a476da13e25fda8f4f8ca54
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2023-09-06 10:19:45 +0200

    proxy service with new exception information

--- src/Core/Rystem.DependencyInjection/ProxyService/ProxyService_1.cs
+++ src/Core/Rystem.DependencyInjection/ProxyService/ProxyService_1.cs
@@ -5,6 +5,21 @@
         protected T? _proxy;
         protected object[]? _parameters;
         protected Type? _proxyType;
-        public T Proxy => _proxy ??= (T)Activator.CreateInstance(_proxyType, _parameters);
+        public T Proxy
+        {
+            get
+            {
+                try
+                {
+                    _proxy ??= (T)Activator.CreateInstance(_proxyType, _parameters);
+                    return _proxy;
+                }
+                catch (Exception ex)
+                {
+                    var counter = 0;
+                    throw new ArgumentException($"Error with instance creation for {_proxyType?.FullName} with parameters length {_parameters?.Length} and types {(_parameters != null ? string.Join(" | ", _parameters.Select(x => x.GetType()?.FullName ?? string.Empty)) : string.Empty)}. Public available constructors {(_proxyType != null ? _proxyType?.GetConstructors().Length : string.Empty)} with parameters {(_proxyType != null ? string.Join($"; {++counter}) ", _proxyType?.GetConstructors().Select(x => string.Join(" | ", x.GetParameters().Select(x => x.ParameterType.FullName)))) : string.Empty)}", ex);
+                }
+            }
+        }
     }
 }
