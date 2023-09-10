6a996b003bf7d7fbceb9280e1671529029a40729
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2023-09-10 23:31:40 +0200

    x

--- src/Repository/RepositoryFramework.Infrastructures/RepositoryFramework.Infrastructure.Azure.Storage.Table/Key/DefaultTableStorageKeyReader.cs
+++ src/Repository/RepositoryFramework.Infrastructures/RepositoryFramework.Infrastructure.Azure.Storage.Table/Key/DefaultTableStorageKeyReader.cs
@@ -6,6 +6,16 @@ namespace RepositoryFramework.Infrastructure.Azure.Storage.Table
     internal sealed class DefaultTableStorageKeyReader<T, TKey> : ITableStorageKeyReader<T, TKey>, IServiceForFactory
         where TKey : notnull
     {
+
+        //todo: this is some that I don't want happens, When i use a factory of an options, I want to have the factory of my options
+        //and now it doesn't work
+        //private TableStorageSettings<T, TKey> _options = null!;
+        //private readonly IFactory<TableClientWrapper<T, TKey>> _factory;
+
+        //public DefaultTableStorageKeyReader(IFactory<TableClientWrapper<T, TKey>> factory)
+        //{
+        //    _factory = factory;
+        //}
         public (string PartitionKey, string RowKey) Read(TKey key, TableStorageSettings<T, TKey> settings)
             => (settings.PartitionKeyFromKeyFunction(key), settings.RowKeyFromKeyFunction?.Invoke(key) ?? string.Empty);
         public TKey Read(T entity, TableStorageSettings<T, TKey> settings)
