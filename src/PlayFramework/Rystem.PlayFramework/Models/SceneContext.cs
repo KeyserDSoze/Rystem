956f01760ffe8ff4c7509b0295aa503c4099c2ae
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2024-10-21 00:10:31 +0200

    Enhance SceneContext and update project version
    Added methods to SceneContext for better property management:
    - GetProperty<T>(object key)
    - TryGetProperty<T>(object key, out T value)
    - GetPropertyOrSetDefault<T>(object key, T? property = default)
    - TrySetProperty<T>(object key, T value)
    - TryOverrideProperty<T>(object key, T value)
    
    Updated version in Rystem.PlayFramework.csproj from 9.0.0-rc.3 to 9.0.0-rc.4.

--- src/PlayFramework/Rystem.PlayFramework/Models/SceneContext.cs
+++ src/PlayFramework/Rystem.PlayFramework/Models/SceneContext.cs
@@ -7,5 +7,67 @@
         public List<SceneIteration> Iterations { get; } = [];
         public IChatClient? CurrentChatClient { get; set; }
         public required Dictionary<object, object> Properties { get; init; }
+        public T GetProperty<T>(object key)
+        {
+            if (Properties.TryGetValue(key, out var propertyValue))
+            {
+                return (T)propertyValue;
+            }
+            else
+            {
+                return default!;
+            }
+        }
+        public bool TryGetProperty<T>(object key, out T value)
+        {
+            if (Properties.TryGetValue(key, out var propertyValue))
+            {
+                value = (T)propertyValue;
+                return true;
+            }
+            else
+            {
+                value = default!;
+                return false;
+            }
+        }
+        public T GetPropertyOrSetDefault<T>(object key, T? property = default)
+        {
+            if (TryGetProperty<T>(key, out var dictionaryValue))
+            {
+                return dictionaryValue;
+            }
+            else
+            {
+                if (Properties.ContainsKey(key))
+                    Properties[key] = property!;
+                else
+                    Properties.Add(key, property!);
+                return property!;
+            }
+        }
+        public bool TrySetProperty<T>(object key, T value)
+        {
+            if (Properties.ContainsKey(key))
+            {
+                return false;
+            }
+            else
+            {
+                return Properties.TryAdd(key, value!);
+            }
+        }
+        public bool TryOverrideProperty<T>(object key, T value)
+        {
+            if (Properties.ContainsKey(key))
+            {
+                Properties[key] = value!;
+            }
+            else
+            {
+                return Properties.TryAdd(key, value!);
+            }
+            return true;
+        }
     }
 }
