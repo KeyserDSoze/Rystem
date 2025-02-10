ab42f56c544e2e9d4d786e0b2ddb2cb34a67614c
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2025-02-10 23:05:59 +0100

    Add LinqExtensions with RemoveWhere method
    Introduces a new static class `LinqExtensions` in the `System.Linq` namespace. The `RemoveWhere` method enables the removal of elements from an `IEnumerable<TSource>` based on a specified predicate, returning the modified list.

---
+++ src/Core/Rystem/System.Linq/LinqExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace System.Linq
+{
+    public static class LinqExtensions
+    {
+        public static IEnumerable<TSource> RemoveWhere<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            var items = source.ToList();
+            for (var i = 0; i < items.Count; i++)
+            {
+                if (predicate(items[i]))
+                {
+                    items.RemoveAt(i);
+                    i--;
+                }
+            }
+            return items;
+        }
+    }
+}
